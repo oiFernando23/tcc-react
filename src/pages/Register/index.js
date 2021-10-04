67b37265370c04dae10a0220e17e5f7c079414c8
Fernando <fernandozardopeixoto@hotmail.com>
2021-10-03 21:52:23 -0300

    03/10

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link, useHistory} from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import api from '../../api'
 
 import "./styles.css";
 
 import logoText from "../../assets/logoText.png";
 
 export default function Register(){
+    const history = useHistory();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+    const [phone, setPhone] = useState('');
+
+    async function handleRegister(e){
+        e.preventDefault();
+        try{
+            await api.post('/users.create', {name, email, phone, password});
+            alert("Usuário cadastrado")
+            history.push('/');
+        }catch(err){
+            alert("Oops, algo deu errado")
+        }
+    }
+
     return (
         <div className="register-container">
             <div className="content">
@@ -21,14 +40,14 @@ export default function Register(){
                     </Link>
                 </section>
 
-                <form action="">
-                    <input placeholder="Nome"/>
-                    <input type="email" placeholder="E-mail"/>
-                    <input placeholder="WhatsApp"/>
-                    <input type="password" placeholder="Senha"/>
+                <form onSubmit={handleRegister}>
+                    <input placeholder="Nome" value={name} onChange={e => setName(e.target.value)}/>
+                    <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="E-mail"/>
+                    <input placeholder="WhatsApp" value={phone} onChange={e => setPhone(e.target.value)}/>
+                    <input type="password" placeholder="Senha" value={password} onChange={e => setPassword(e.target.value)}/>
                     <button className="button" type="submit">Cadastrar</button>
                 </form>
             </div>
         </div>
-    );
+    )
 }
